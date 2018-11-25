0401c7bbfd77faba3f957eff8366d9d195238ce6
Sukang Sabina Kim <sukang@umich.edu>
2018-11-25 00:40:45 -0500

    Updates

---
+++ precache-manifest.4c6c14f65d03dbaeb299449ec63130d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eae404c575c46e846cba3f2903ced72",
+    "url": "/static/media/flower_paint.1eae404c.jpeg"
+  },
+  {
+    "revision": "407a613f3976d0058d17",
+    "url": "/static/css/main.1aa9e28e.chunk.css"
+  },
+  {
+    "revision": "4efd4767e1f6bbf3fb2f",
+    "url": "/static/js/1.4efd4767.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2956f4e0e0ae171428211bbd7b16c3cc",
+    "url": "/static/media/email.2956f4e0.png"
+  },
+  {
+    "revision": "ba665dff072c2b069e3d17acb804d0e3",
+    "url": "/static/media/linkedIn.ba665dff.png"
+  },
+  {
+    "revision": "d9268123f113c5cc9924375b79a1aac2",
+    "url": "/static/media/window.d9268123.jpeg"
+  },
+  {
+    "revision": "6ff9a3ad1c41869ec50b773b892f7f6c",
+    "url": "/static/media/bicycle.6ff9a3ad.jpeg"
+  },
+  {
+    "revision": "407a613f3976d0058d17",
+    "url": "/static/js/main.407a613f.chunk.js"
+  },
+  {
+    "revision": "de215b566a0b7911cc6abc475d4f7140",
+    "url": "/static/media/flower_restaurant.de215b56.jpeg"
+  },
+  {
+    "revision": "ac45e1ba7fa3fde95e6a75917d82fe16",
+    "url": "/static/media/sculpture.ac45e1ba.jpeg"
+  },
+  {
+    "revision": "ffa286030569203823ceb09ceaed4efb",
+    "url": "/static/media/flower_street.ffa28603.jpeg"
+  },
+  {
+    "revision": "17430a81361a0ffdbf4fe4900743fab0",
+    "url": "/static/media/cropped-me.17430a81.gif"
+  },
+  {
+    "revision": "0eaa4c149ee2c5c7a127e6fc62bebadb",
+    "url": "/static/media/caen.0eaa4c14.png"
+  },
+  {
+    "revision": "77b62395a356a63f7b91945ab0282c8c",
+    "url": "/static/media/tub.77b62395.png"
+  },
+  {
+    "revision": "5eff3516ea45cfcc93225a01f99df775",
+    "url": "/static/media/ksea.5eff3516.png"
+  },
+  {
+    "revision": "1271af0668ab2c5a6697f96e2e4d42e4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.77f4af56452b938872c5017124754b32.js"
+  "/precache-manifest.4c6c14f65d03dbaeb299449ec63130d0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
