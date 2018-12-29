8a6b511728d63c711922b3d4a7869e8d81a00a6b
Sukang Sabina Kim <sukang@umich.edu>
2018-12-29 20:59:51 +0900

    Updates

---
+++ precache-manifest.dcea1fa7c0c1f0ea0ac5d9f73bc37700.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac45e1ba7fa3fde95e6a75917d82fe16",
+    "url": "/static/media/sculpture.ac45e1ba.jpeg"
+  },
+  {
+    "revision": "4503dd98ff393476dbf7",
+    "url": "/static/css/main.2038aaa2.chunk.css"
+  },
+  {
+    "revision": "b3a80c1aeeddf56c9047",
+    "url": "/static/js/1.b3a80c1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2956f4e0e0ae171428211bbd7b16c3cc",
+    "url": "/static/media/email.2956f4e0.png"
+  },
+  {
+    "revision": "ba665dff072c2b069e3d17acb804d0e3",
+    "url": "/static/media/linkedIn.ba665dff.png"
+  },
+  {
+    "revision": "d9268123f113c5cc9924375b79a1aac2",
+    "url": "/static/media/window.d9268123.jpeg"
+  },
+  {
+    "revision": "6ff9a3ad1c41869ec50b773b892f7f6c",
+    "url": "/static/media/bicycle.6ff9a3ad.jpeg"
+  },
+  {
+    "revision": "1eae404c575c46e846cba3f2903ced72",
+    "url": "/static/media/flower_paint.1eae404c.jpeg"
+  },
+  {
+    "revision": "de215b566a0b7911cc6abc475d4f7140",
+    "url": "/static/media/flower_restaurant.de215b56.jpeg"
+  },
+  {
+    "revision": "4503dd98ff393476dbf7",
+    "url": "/static/js/main.4503dd98.chunk.js"
+  },
+  {
+    "revision": "ffa286030569203823ceb09ceaed4efb",
+    "url": "/static/media/flower_street.ffa28603.jpeg"
+  },
+  {
+    "revision": "17430a81361a0ffdbf4fe4900743fab0",
+    "url": "/static/media/cropped-me.17430a81.gif"
+  },
+  {
+    "revision": "7a69146fe728ee7a97af1f6e44914737",
+    "url": "/static/media/eat_main.7a69146f.jpeg"
+  },
+  {
+    "revision": "1bc73574855bbb5bd8face39abf98a87",
+    "url": "/static/media/eat_list.1bc73574.jpeg"
+  },
+  {
+    "revision": "0eaa4c149ee2c5c7a127e6fc62bebadb",
+    "url": "/static/media/caen.0eaa4c14.png"
+  },
+  {
+    "revision": "77b62395a356a63f7b91945ab0282c8c",
+    "url": "/static/media/tub.77b62395.png"
+  },
+  {
+    "revision": "5eff3516ea45cfcc93225a01f99df775",
+    "url": "/static/media/ksea.5eff3516.png"
+  },
+  {
+    "revision": "2a27921b357b9820713fe9f127d9d29f",
+    "url": "/static/media/mEngin.2a27921b.jpeg"
+  },
+  {
+    "revision": "97839430a86389ae9e406f7eefb4625a",
+    "url": "/index.html"
+  }
+];
